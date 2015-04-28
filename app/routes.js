e082e4b92f254f67784399784e7cd22d25085805
Dom Smith <dom@teneightfive.com>
2015-04-28 14:45:32 +0100

    Improve juror login validation

--- app/routes.js
+++ app/routes.js
@@ -8,14 +8,25 @@ module.exports = {
     app.get('/login', function (req, res) {
       var YAML = require('yamljs');
       var jurors = YAML.load(__dirname + '/../data/jurors.yml');
-      var jurorNumber = req.query.jurorNumber;
+      var jurorNumber = req.query.jurorNumber.trim();
 
-      if (jurors.indexOf(jurorNumber) > -1) {
+      if (jurors.indexOf(jurorNumber.replace(/ /g,'')) > -1) {
         res.redirect('/your-service');
       } else {
-        var errors = {
-          jurorNumber: 'Your juror number is wrong'
+        var errors = {};
+
+        if (jurorNumber === undefined || jurorNumber === '') {
+          errors.jurorNumber = {
+            fieldName: 'Juror number',
+            msg: 'cannot be blank'
+          }
+        } else {
+          errors.jurorNumber = {
+            fieldName: 'Juror number',
+            msg: 'is either invalid or not on our system'
+          }
         }
+
         res.render('login', {
           'errors': errors,
           'jurorNumber': jurorNumber
