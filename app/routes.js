c79cb0ee0539ab4ab5450afe31801a45ff4244de
Dom Smith <dom@teneightfive.com>
2015-04-28 12:28:44 +0100

    Add basic login ability to check juror number exists

--- app/routes.js
+++ app/routes.js
@@ -2,11 +2,25 @@ module.exports = {
   bind : function (app) {
 
     app.get('/', function (req, res) {
-      res.render('index');
+      res.render('login');
     });
 
-    app.get('/examples/template-data', function (req, res) {
-      res.render('examples/template-data', { 'name' : 'Foo' });
+    app.get('/login', function (req, res) {
+      var YAML = require('yamljs');
+      var jurors = YAML.load(__dirname + '/../data/jurors.yml');
+      var jurorNumber = req.query.jurorNumber;
+
+      if (jurors.indexOf(jurorNumber) > -1) {
+        res.redirect('/your-service');
+      } else {
+        var errors = {
+          jurorNumber: 'Your juror number is wrong'
+        }
+        res.render('login', {
+          'errors': errors,
+          'jurorNumber': jurorNumber
+        });
+      }
     });
 
     // add your routes here
